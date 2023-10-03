f415f5b097e6a60fc153d85a7798a7d09c94058c
andreavirgilio99 <adrea.virgilio@tsigroupsrl.it>
2023-10-03 16:42:32 +0200

    l.,+

--- src/app/components/signer/signer.component.ts
+++ src/app/components/signer/signer.component.ts
@@ -1,8 +1,8 @@
 import { Component, ElementRef, Input, OnChanges, ViewChild } from '@angular/core';
 import * as pdfjsLib from 'pdfjs-dist';
 
-type Coordinates = {x: number, y: number}
-type Segment = {points: Coordinates[]}
+type Point = { x: number, y: number };
+type Segment = { points: Point[], color: string, width: number };
 
 @Component({
   selector: 'app-signer',
@@ -21,9 +21,10 @@ export class SignerComponent implements OnChanges {
   lastX = 0;
   lastY = 0;
   selectedColor = '#000000'; // Default color is black
+  selectedWidth = 2; // Default width is 2
   segments = new Map<number, Segment[]>()
 
-  currentSegment: Coordinates[] = []
+  currentSegment: Point[] = []
 
   constructor() {
     pdfjsLib.GlobalWorkerOptions.workerSrc = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.0.279/pdf.worker.min.js"
@@ -64,9 +65,9 @@ export class SignerComponent implements OnChanges {
       page.render({ canvasContext: this.ctx!, viewport }).promise.then(() => {
         // Ripristina i disegni della pagina corrente
         const currPageLines = this.segments.get(pageNumber);
-        if(currPageLines){
-          currPageLines.forEach(line =>{
-            line.points.forEach(point => this.drawPoint(point))
+        if (currPageLines) {
+          currPageLines.forEach(line => {
+            line.points.forEach(point => this.drawPoint(point,line.color, line.width))
           })
         }
       }).catch(error => {
@@ -105,15 +106,22 @@ export class SignerComponent implements OnChanges {
 
   draw(event: MouseEvent) {
     if (!this.drawing) return;
-    //usa il colore selezionato per disegnare
+  
+    const x = event.offsetX;
+    const y = event.offsetY;
+  
+    // Usa il colore selezionato per disegnare
     this.ctx!.globalCompositeOperation = 'source-over';
     this.ctx!.strokeStyle = this.selectedColor;
-    this.ctx!.lineWidth = 2; // Imposta la larghezza del tratto
-
-    // Disegna il tratto
-    this.ctx!.lineTo(event.offsetX, event.offsetY);
+    this.ctx!.lineWidth = this.selectedWidth;
+  
+    // Disegna una linea continua tra i punti consecutivi
+    this.ctx!.lineTo(x, y);
     this.ctx!.stroke();
-    this.currentSegment.push({x: event.offsetX, y: event.offsetY})
+    this.ctx!.beginPath();
+    this.ctx!.moveTo(x, y);
+  
+    this.currentSegment.push({ x, y });
   }
 
   // Stop drawing
@@ -121,31 +129,31 @@ export class SignerComponent implements OnChanges {
     this.drawing = false;
     this.ctx!.closePath();
     const clone = JSON.parse(JSON.stringify(this.currentSegment))
-    const newSegment: Segment = {points: clone}
+    const newSegment: Segment = { points: clone, color: this.selectedColor, width: this.selectedWidth }
 
-    if(this.segments.get(this.currentPage) == undefined){
+    if (this.segments.get(this.currentPage) == undefined) {
       this.segments.set(this.currentPage, [newSegment])
     }
-    else{
+    else {
       this.segments.get(this.currentPage)!.push(newSegment)
     }
     console.log(this.segments.get(this.currentPage))
     this.currentSegment = []
   }
 
-  mouseLeaveCheck(){
-    if(this.drawing){
+  mouseLeaveCheck() {
+    if (this.drawing) {
       this.endDrawing()
     }
   }
 
   // Aggiungi questa funzione al tuo componente
-  drawPoint(point: Coordinates) {
+  drawPoint(point: Point, color: string, width: number) {
     if (!this.ctx) return;
-
+  
     this.ctx.beginPath();
-    this.ctx.arc(point.x, point.y, 2, 0, 2 * Math.PI);
-    this.ctx.fillStyle = this.selectedColor;
+    this.ctx.arc(point.x, point.y, width / 2, 0, 2 * Math.PI);
+    this.ctx.fillStyle = color;
     this.ctx.fill();
   }
 
@@ -155,12 +163,12 @@ export class SignerComponent implements OnChanges {
   }
 
   undoLastAction() {
-   const currSegments = this.segments.get(this.currentPage)
+    const currSegments = this.segments.get(this.currentPage)
 
-   if(currSegments && currSegments.length > 0){
-    currSegments.pop()
-    this.renderPage(this.currentPage);
-   }
+    if (currSegments && currSegments.length > 0) {
+      currSegments.pop()
+      this.renderPage(this.currentPage);
+    }
   }
 
   downloadModifiedPDF() {
